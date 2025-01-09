ddebc9e2c1e38fed685237630f53920eeabd5668
chikkka <141250393+chikkka@users.noreply.github.com>
2025-01-09 13:31:59 +0900

    add html / script

--- script.js
+++ script.js
@@ -6,16 +6,33 @@ btn.onclick = () => {
   nav.classList.toggle("open");
 };
 
+// 下にスクロールするとtopに戻るボタンが出現する
+const returnTopButton = document.getElementById("returnTop");
+const title = document.getElementById("subTitle");
+const setPoint = title.offsetTop;
+window.addEventListener("scroll",()=>{
+  if(window.scrollY >= setPoint){
+    returnTopButton.classList.add("fixed");
+  }else{
+    returnTopButton.classList.remove("fixed");
+  }
+});
+
+
+
+// -------menuページ---------
 // メニューバーの固定
-const menuBar = document.querySelector("#effectItem");
+const menuBar = document.getElementById("effectItem");
 const stickyPoint = menuBar.offsetTop;
 window.addEventListener("scroll",()=>{
   if(window.scrollY >= stickyPoint){
   menuBar.classList.add("fixed");
-}else{
+  }else{
   menuBar.classList.remove("fixed");
-}
+  }
 });
+
+
 
 
 
